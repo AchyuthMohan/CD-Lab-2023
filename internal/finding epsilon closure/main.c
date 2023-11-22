cb3352faabc04db448b82f52d2b33cda1fe40070
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-11-22 21:30:53 +0530

    all set

--- internal/finding epsilon closure/main.c
+++ internal/finding epsilon closure/main.c
@@ -1,57 +1,51 @@
-// Program to find epsilon closure of a given NFA
-
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
-
-char result[20][20],copy[3],states[20][20];
-
-void add_state(char a[3],int i){
-	strcpy(result[i],a);
+#define max 20
+int n,i;
+FILE *fp;
+char states[max][max],result[max][max],copy[3],state[3],state1[3],state2[3],input[3];
+void addState(char a[3],int q){
+    strcpy(result[q],a);
 }
 
 void display(int n){
-	int k=0;
-	printf("nnn Epsilon closure of %s = { ",copy);
-	while(k < n){
-		printf(" %s",result[k]);
-		k++;
-	}
-	printf(" } nnn");
+   int k=0;
+   printf("Epsilon closure of %s = {",copy);
+   while(k<n){
+    printf("%s,",result[k]);
+    k++;
+   }
+   printf("}");
 }
-
 int main(){
-    FILE *INPUT;
-    INPUT=fopen("input.dat","r");
-    char state[3];
-    int end,i=0,n,k=0;
-    char state1[3],input[3],state2[3];
-    printf("n Enter the no of states: ");
+    fp=fopen("input.dat","r");
+    printf("Enter the number of states: \n");
     scanf("%d",&n);
-    printf("n Enter the states n");
-    for(k=0;k<n;k++){
-		scanf("%s",states[k]);
-	}
-	for( k=0;k<n;k++){
-		i=0;
-		strcpy(state,states[k]);
-		strcpy(copy,state);
-		add_state(state,i++);
-		while(1){
-			end = fscanf(INPUT,"%s%s%s",state1,input,state2);
-			if (end == EOF ){
-				break;
-			}
-				
-			if( strcmp(state,state1) == 0 ){
-				if( strcmp(input,"e") == 0 ) {
-					add_state(state2,i++);
-					strcpy(state, state2);
-				}
-			}
+    printf("Enter the states: \n");
+    for(int k=0;k<n;k++){
+        scanf("%s",states[k]);
+    }
+    int end;
 
-		}
-		display(i);
-	}
-  
-    return 0;
+    for(int k=0;k<n;k++){
+        i=0;
+        strcpy(state,states[k]);
+        strcpy(copy,state);
+        addState(state,i++);
+        while(1){
+            end=fscanf(fp,"%s%s%s",state1,input,state2);
+            if(end==EOF){
+                break;
+            }
+            if(strcmp(state1,state)==0){
+                if(strcmp(input,"e")==0){
+                    strcpy(state,state2);
+                    addState(state2,i++);
+                }
+            }
+        }
+        display(i);
+    }
+
 }
