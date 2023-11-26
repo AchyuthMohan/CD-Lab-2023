09ddff900598d678886b9d5446d451498e2faccf
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-11-26 20:04:47 +0530

    name removal  corrected

--- internal/e-NFA to NFA/main.c
+++ internal/e-NFA to NFA/main.c
@@ -1,67 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_STATES 100
-#define MAX_SYMBOLS 100
-struct Transition {
-    int src;        // Source state
-    char symbol;    // Transition symbol (epsilon is represented as '\0')
-    int dest;       // Destination state
-};
-void epsilonClosure(int nfa[MAX_STATES][MAX_SYMBOLS], int n, int epsilonClosureSet[MAX_STATES][MAX_STATES]) {
-    int i, j, k;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            epsilonClosureSet[i][j] = nfa[i][j];
+#define max 20
+int nfa[max][max];
+void epsilonConvert(int nfa[][], int n, int nfaWithEpsilonTrans[][])
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            nfaWithEpsilonTrans[i][j] = nfa[i][j];
         }
     }
-    for (k = 0; k < n; k++) {
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                epsilonClosureSet[i][j] = epsilonClosureSet[i][j] || (epsilonClosureSet[i][k] && epsilonClosureSet[k][j]);
+    for (int k = 0; k < nk++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                nfaWithEpsilonTrans[i][j] = nfaWithEpsilonTrans[i][j] || (nfaWithEpsilonTrans[i][k] && nfaWithEpsilonTrans[k][j])
             }
         }
     }
 }
-
-int main() {
-    int n; // Number of states
-    int m; // Number of symbols
-    int i, j, k;
-    printf("Enter the number of states: ");
+int main()
+{
+    int n, m;
+    printf("Enter the number of states: \n");
     scanf("%d", &n);
-    printf("Enter the number of symbols: ");
-    scanf("%d", &m);
-    int nfa[MAX_STATES][MAX_SYMBOLS];
-    printf("Enter the NFA transition table with epsilon transitions (use -1 for no transition):\n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
-            printf("Transition from state %d with symbol %d (-1 if no transition): ", i, j);
-            scanf("%d", &nfa[i][j]);
+    printf("Enter the number of inputs: \n");
+    scanf("%d", &m) for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf("Enter the destination state from state %d during input %d: \n", i, j);
+            scanf("%d", &nfa[i][j])
         }
     }
-    printf("\nOriginal NFA with epsilon transitions:\n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
-            printf("(%d, %d, %d) ", i, j, nfa[i][j]);
-        }
-        printf("\n");
-    }
-
-    int epsilonClosureSet[MAX_STATES][MAX_STATES];
-    epsilonClosure(nfa, n, epsilonClosureSet);
-    int newNFA[MAX_STATES][MAX_SYMBOLS];
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
-            newNFA[i][j] = epsilonClosureSet[i][nfa[i][j]];
+    int nfaWithEpsilonTrans[max][max];
+    epsilonConvert(nfa,n,nfaWithEpsilonTrans);
+    int resNfa[max][max];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            resNfa[i][j]=nfaWithEpsilonTrans[i][nfa[i][j]];
         }
     }
-    printf("\nNFA without epsilon transitions:\n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
-            printf("(%d, %d, %d) ", i, j, newNFA[i][j]);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            printf("(%d,%d,%d)\n",i,j,resNfa[i][j])
         }
-        printf("\n");
     }
-
-    return 0;
 }
