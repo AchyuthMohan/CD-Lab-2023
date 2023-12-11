dde64ba95f06ad2bc523d40dcfe25250077e1312
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-12-11 13:13:14 +0530

    first and follow done

--- external/first follow/main.c
+++ external/first follow/main.c
@@ -1,78 +1,66 @@
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
-int n, m = 0, p, i = 0, j = 0;
-char a[10][10], f[10];
-void first(char c)
-{
-    int k;
-    if (!isupper(c))
-        f[m++] = c;
-    for (k = 0; k < n; k++)
-    {
-        if (a[k][0] == c)
-        {
-            if (a[k][2] == '$')
-                follow(a[k][0]);
-            else if (islower(a[k][2]))
-                f[m++] = a[k][2];
-            else
-                first(a[k][2]);
-        }
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+#define max 20
+
+char f[max],a[max][max];
+int m=0,n;
+
+void first(char ch){
+    if(islower(ch)){
+        f[m++]=ch;
     }
-}
-void follow(char c)
-{
-    if (a[0][0] == c)    //start symbol
-        f[m++] = '$';
-    for (i = 0; i < n; i++)
-    {
-        for (j = 2; j < strlen(a[i]); j++)
-        {
-            if (a[i][j] == c)
-            {
-                if (a[i][j + 1] != '\0')
-                    first(a[i][j + 1]);
-                if (a[i][j + 1] == '\0' && c != a[i][0])
-                    follow(a[i][0]);
+    for(int i=0;i<n;i++){
+        if(a[i][0]==ch){
+            if(a[i][2]=='$'){
+                follow(ch);
+            }
+            else if(islower(a[i][2])){
+                f[m++]=a[i][2];
+            }
+            else{
+                first(a[i][2]);
             }
         }
     }
 }
 
-int main()
-{
+void follow(char ch){
+    if(ch==a[0][0]){
+        f[m++]='$';
+    }
+    for(int i=0;i<n;i++){
+        for(int j=2;j<strlen(a[i]);j++){
+            if(a[i][j]==ch){
+                if(a[i][j+1]!='\0'){
+                    first(a[i][j+1]);
+                }
+                else if(a[i][j+1]=='\0' && a[i][0]!=ch){
+                    follow(a[i][0]);
+                }
+            }
+        }
+    }
+}
 
-    int i, z;
-    char c, ch;
-    // clrscr();
-    printf("Enter the no of prooductions:\n");
-    scanf("%d", &n);
-    printf("Enter the productions:\n");
-    for (i = 0; i < n; i++)
-        scanf("%s%c", a[i], &ch);
-    do
-    {
-        m = 0;
-        printf("Enter the elemets whose fisrt & follow is to be found:");
-        scanf("%c", &c);
-        first(c);
-        printf("First(%c)={", c);
-        for (i = 0; i < m; i++)
-            printf("%c", f[i]);
-        printf("}\n");
-        strcpy(f, " ");
-        // flushall();
-        m = 0;
-        follow(c);
-        printf("Follow(%c)={", c);
-        for (i = 0; i < m; i++)
-            printf("%c", f[i]);
-        printf("}\n");
-        printf("Continue(0/1)?");
-        scanf("%d%c", &z, &ch);
-    } while (z == 1);
-    return (0);
+int main(){
+    printf("Enter the number of productions: \n");
+    scanf("%d",&n);
+    printf("Enter the grammar: \n");
+    for(int i=0;i<n;i++){
+        scanf("%s",a[i]);
+    }
+    while(1){
+        printf("Enter the symbol whose first and follow has to be calculated: \n");
+        char ch;
+        scanf("%c",&ch);
+        first(ch);
+        printf("First(%c)= { %c ,",ch,ch );
+        for(int i=0;i<m;i++){
+            printf("%c, ",f[i]);
+        }
+        printf("}");
+    }
 }
