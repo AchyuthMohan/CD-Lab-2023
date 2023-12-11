8b6f566eb8feaf0d5afd457b85553cfaa7bda5c7
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-12-11 21:56:38 +0530

    code

--- external/recursive descent/main.c
+++ external/recursive descent/main.c
@@ -1,111 +1,120 @@
-#include <stdio.h>  
-#include <string.h>  
-#define SUCCESS 1  
-#define FAILED 0  
-  
-int E(), Edash(), T(), Tdash(), F();  
-  
-const char *cursor;  
-char string[64];  
-  
-int main()  
-{  
-    puts("Enter the string");  
-    scanf("%s", string);  
-    cursor = string;  
-    puts("");  
-    puts("Input      Action");  
-    puts("--------------------------------");  
-  
-    if (E() && *cursor == '\0') {  
-        puts("--------------------------------");  
-        puts("String is successfully parsed");  
-        return 0;  
-    } else {  
-        puts("--------------------------------");  
-        puts("Error in parsing String");  
+#include <stdio.h>
+#include <stdlib.h>
+
+char string[64];
+const char *cursor;
+
+int E(), EDash(), T(), TDash(), F();
+
+int main()
+{
+    printf("Enter the input string: ");
+    scanf("%s", string);
+    cursor = string;
+    if (E() && *cursor == '\0')
+    {
+        printf("Accepted...");
+        return 0;
+    }
+    else
+    {
+        printf("String not accepted..\n");
+        exit(0);
+    }
+}
+int E()
+{
+    printf("%s E->TE'\n", cursor);
+    if (T())
+    {
+        if (EDash())
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else
+    {
+        return 0;
+    }
+}
+int EDash()
+{
+    if (*cursor == '+')
+    {
+        printf("%-16s E' -> + T E'\n", cursor);
+        cursor++;
+        if (T())
+        {
+            if (EDash())
+                return 1;
+            else
+                return 0;
+        }
+        else
+            return 0;
+    }
+    else
+    {
+        printf("%-16s E' -> $\n", cursor);
+        return 1;
+    }
+}
+int T()
+{
+    printf("%-16s T -> F T'\n", cursor);  
+    if (F())
+    {
+        if (TDash())
+        {
+            return 1;
+        }
+        return 0;
+    }
+    return 0;
+}
+int TDash()
+{
+    if (*cursor == '*')
+    {
+        printf("%s T'->*FT'\n", cursor);
+        cursor++;
+        if (F())
+        {
+            if (TDash())
+            {
+                return 1;
+            }
+            return 0;
+        }
+        return 0;
+    }
+    else {  
+        printf("%-16s T' -> $\n", cursor);  
         return 1;  
     }  
-}  
-  
-int E()  
-{  
-    printf("%s E -> T E'\n", cursor);  
-    if (T()) {  
-        if (Edash())  
-            return SUCCESS;  
-        else  
-            return FAILED;  
-    } else  
-        return FAILED;  
-}  
-  
-int Edash()  
-{  
-    if (*cursor == '+') {  
-        printf("%s E' -> + T E'\n", cursor);  
-        cursor++;  
-        if (T()) {  
-            if (Edash())  
-                return SUCCESS;  
-            else  
-                return FAILED;  
-        } else  
-            return FAILED;  
-    } else {  
-        printf("%s E' -> $\n", cursor);  
-        return SUCCESS;  
-    }  
-}  
-  
-int T()  
-{  
-    printf("%s T -> F T'\n", cursor);  
-    if (F()) {  
-        if (Tdash())  
-            return SUCCESS;  
-        else  
-            return FAILED;  
-    } else  
-        return FAILED;  
-}  
-  
-int Tdash()  
-{  
-    if (*cursor == '*') {  
-        printf("%s T' -> * F T'\n", cursor);  
-        cursor++;  
-        if (F()) {  
-            if (Tdash())  
-                return SUCCESS;  
-            else  
-                return FAILED;  
-        } else  
-            return FAILED;  
-    } 
-    else {  
-        printf("%s T' -> $\n", cursor);  
-        return SUCCESS;  
-    }  
-}  
-  
-int F()  
-{  
-    if (*cursor == '(') {  
-        printf("%s F -> ( E )\n", cursor);  
+    
+}
+int F()
+{
+     if (*cursor == '(') {  
+        printf("%-16s F -> ( E )\n", cursor);  
         cursor++;  
         if (E()) {  
             if (*cursor == ')') {  
                 cursor++;  
-                return SUCCESS;  
+                return 1;  
             } else  
-                return FAILED;  
+                return 0;  
         } else  
-            return FAILED;  
+            return 0;  
     } else if (*cursor == 'i') {  
         cursor++;  
-        printf("%s F ->i\n", cursor);  
-        return SUCCESS;  
+        printf("%-16s F ->i\n", cursor);  
+        return 1;  
     } else  
-        return FAILED;  
-}  
+        return 0;  
+}
