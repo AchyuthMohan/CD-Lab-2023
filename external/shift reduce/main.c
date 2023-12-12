0e25ee711dfe78f108a98f659dd6c8542fc971d1
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-12-12 12:03:39 +0530

    done

--- external/shift reduce/main.c
+++ external/shift reduce/main.c
@@ -1,83 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-char ip[30], stk[30], act[30], ac[30];
-int i = 0, j = 0, n, z = 0;
-
-void check()
-{
-    for (z = 0; z < n; z++)
-    {
-        if (stk[z] == 'i' && stk[z + 1] == 'd')
-        {
-            stk[z] = 'E';
-            stk[z + 1] = '\0';
+char stack[30],ip[30],act[20],ac[20];
+int i=0,j=0,z=0,n;
+void check(){
+    for(z=0;z<n;z++){
+        if(stack[z]=='i' && stack[z+1]=='d'){
+            stack[z]='E';
+            stack[z+1]='\0';
+            printf("%s\t\t%s\t\t%s\n",stack,ip,ac);
             j++;
-            printf("$%s\t\t\t%s\t\t\t%s\n", stk, ip, ac);
         }
     }
-
-    for (z = 0; z < n; z++)
-    {
-        if (stk[z] == 'E' && stk[z + 1] == '+' && stk[z + 2] == 'E')
-        {
-            stk[z] = 'E';
-            stk[z + 1] = '\0';
-            stk[z + 1] = '\0';
-            printf("$%s\t\t\t%s\t\t\t%s\n", stk, ip, ac);
-            i = i - 2;
+    for(z=0;z<n;z++){
+        if(stack[z]=='E' && stack[z+1]=='+' && stack[z+2]=='E'){
+            stack[z]='E';
+            stack[z+1]='\0';
+            stack[z+2]='\0';
+            printf("%s\t\t%s\t\t%s\n",stack,ip,ac);
+            i=i-1;
         }
     }
-    for (z = 0; z < n; z++)
-    {
-        if (stk[z] == 'E' && stk[z + 1] == '*' && stk[z + 2] == 'E')
-        {
-            stk[z] = 'E';
-            stk[z + 1] = '\0';
-            stk[z + 1] = '\0';
-            printf("$%s\t\t\t%s\t\t\t%s\n", stk, ip, ac);
-            i = i - 2;
+    for(z=0;z<n;z++){
+        if(stack[z]=='E' && stack[z+1]=='*' && stack[z+2]=='E'){
+            stack[z]='E';
+            stack[z+1]='\0';
+            stack[z+2]='\0';
+            printf("%s\t\t%s\t\t%s\n",stack,ip,ac);
+            i=i-1;
         }
     }
-    for (z = 0; z < n; z++)
-    {
-        if (stk[z] == '(' && stk[z + 1] == 'E' && stk[z + 2] == ')')
-        {
-            stk[z] = 'E';
-            stk[z + 1] = '\0';
-            stk[z + 1] = '\0';
-            printf("$%s\t\t\t%s\t\t\t%s\n", stk, ip, ac);
-            i = i - 2;
+    for(z=0;z<n;z++){
+        if(stack[z]=='(' && stack[z+1]=='E' && stack[z+2]==')'){
+            stack[z]='E';
+            stack[z+1]='\0';
+            stack[z+2]='\0';
+            printf("%s\t\t%s\t\t%s\n",stack,ip,ac);
+            i=i-1;
         }
     }
 }
-int main()
-{
-    printf("Gramar\n E->E+E\nE->E*E\nE->(E)\nE->id\n");
-    printf("Enter the string input: ");
-    scanf("%s", ip);
-    strcpy(act, "SHIFT");
-    strcpy(ac, "REDUCE to E");
-    printf("\nstck\t\t\tinput\t\t\taction\n");
-    n = strlen(ip);
-    for (i = 0, j = 0; j < n; i++, j++)
-    {
-        if (ip[j] == 'i' && ip[i + 1] == 'd')
-        {
-            stk[i] = ip[j];
-            stk[i + 1] = ip[j + 1];
-            stk[i + 2] = '\0';
-            ip[i] = ip[i + 1] = ' ';
-            printf("$%s\t\t\t%s\t\t\t%s\n", stk, ip, act);
+int main(){
+    printf("Grammar: \n E->E+E\nE->E*E\nE->(E)\nE->id\n");
+    printf("enter the string input: ");
+    scanf("%s",ip);
+    n=strlen(ip);
+    strcpy(act,"SHIFT symbol");
+    strcpy(ac,"REDUCE E");
+    printf("STACK\t\tINPUT\t\tACTION\n");
+    for(i=0,j=0;j<n;i++,j++){
+        if(ip[j]=='i' && ip[j+1]=='d'){
+            stack[i]=ip[j];
+            stack[i+1]=ip[j+1];
+            stack[i+2]='\0';
+            ip[i]=ip[i+1]=' ';
+            printf("%s\t\t%s\t\t%s\n",stack,ip,act);
             check();
         }
-        else
-        {
-            stk[i] = ip[j];
-            stk[i + 1] = '\0';
-            ip[j] = ' ';
-            printf("$%s\t\t\t%s\t\t\t%s\n", stk, ip, act);
+        else{
+            stack[i]=ip[j];
+            ip[j]=' ';
+            stack[i+1]='\0';
+            printf("%s\t\t%s\t\t%s\n",stack,ip,act);
             check();
         }
     }
