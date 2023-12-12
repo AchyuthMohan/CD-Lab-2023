b5c59dc50bdc356c52a91bb7469f73b22ac610ee
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-12-12 11:09:29 +0530

    done

--- external/shift reduce/main.c
+++ external/shift reduce/main.c
@@ -1,86 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-int k = 0, z = 0, i = 0, j = 0, c = 0;
-char a[16], ac[20], stk[15], act[10];
-void check();
-int main()
-{
 
-    puts("GRAMMAR is E->E+E \n E->E*E \n E->(E) \n E->id");
-    puts("enter input string ");
-    scanf("%s", a);
-    c = strlen(a);
-    strcpy(act, "SHIFT->");
-    puts("stack \t input \t action");
-    for ( i = 0,j=0; j < c; i++, j++)
-    {
-        if (a[j] == 'i' && a[j + 1] == 'd')
-        {
-            stk[i] = a[j];
-            stk[i + 1] = a[j + 1];
-            stk[i + 2] = '\0';
-            a[j] = ' ';
-            a[j + 1] = ' ';
-            printf("\n$%s\t%s$\t%sid", stk, a, act);
-            check();
-        }
-        else
-        {
-            stk[i] = a[j];
-            stk[i + 1] = '\0';
-            a[j] = ' ';
-            printf("\n$%s\t%s$\t%ssymbols", stk, a, act);
-            check();
-        }
-    }
-}
+char ip[30], stk[30], act[30], ac[30];
+int i = 0, j = 0, n, z = 0;
+
 void check()
 {
-    strcpy(ac, "REDUCE TO E");
-    for (z = 0; z < c; z++)
+    for (z = 0; z < n; z++)
     {
         if (stk[z] == 'i' && stk[z + 1] == 'd')
         {
             stk[z] = 'E';
             stk[z + 1] = '\0';
-            printf("\n$%s\t%s$\t%s", stk, a, ac);
             j++;
+            printf("$%s\t\t\t%s\t\t\t%s\n", stk, ip, ac);
         }
     }
 
-    for (z = 0; z < c; z++)
+    for (z = 0; z < n; z++)
     {
         if (stk[z] == 'E' && stk[z + 1] == '+' && stk[z + 2] == 'E')
         {
             stk[z] = 'E';
             stk[z + 1] = '\0';
-            stk[z + 2] = '\0';
-            printf("\n$%s\t%s$\t%s", stk, a, ac);
+            stk[z + 1] = '\0';
+            printf("$%s\t\t\t%s\t\t\t%s\n", stk, ip, ac);
             i = i - 2;
         }
     }
-
-    for (z = 0; z < c; z++)
+    for (z = 0; z < n; z++)
     {
         if (stk[z] == 'E' && stk[z + 1] == '*' && stk[z + 2] == 'E')
         {
             stk[z] = 'E';
             stk[z + 1] = '\0';
             stk[z + 1] = '\0';
-            printf("\n$%s\t%s$\t%s", stk, a, ac);
+            printf("$%s\t\t\t%s\t\t\t%s\n", stk, ip, ac);
             i = i - 2;
         }
     }
-
-    for (z = 0; z < c; z++)
+    for (z = 0; z < n; z++)
     {
         if (stk[z] == '(' && stk[z + 1] == 'E' && stk[z + 2] == ')')
         {
             stk[z] = 'E';
             stk[z + 1] = '\0';
             stk[z + 1] = '\0';
-            printf("\n$%s\t%s$\t%s", stk, a, ac);
+            printf("$%s\t\t\t%s\t\t\t%s\n", stk, ip, ac);
             i = i - 2;
+        }
+    }
+}
+int main()
+{
+    printf("Gramar\n E->E+E\nE->E*E\nE->(E)\nE->id\n");
+    printf("Enter the string input: ");
+    scanf("%s", ip);
+    strcpy(act, "SHIFT");
+    strcpy(ac, "REDUCE to E");
+    printf("\nstck\t\t\tinput\t\t\taction\n");
+    n = strlen(ip);
+    for (i = 0, j = 0; j < n; i++, j++)
+    {
+        if (ip[j] == 'i' && ip[i + 1] == 'd')
+        {
+            stk[i] = ip[j];
+            stk[i + 1] = ip[j + 1];
+            stk[i + 2] = '\0';
+            ip[i] = ip[i + 1] = ' ';
+            printf("$%s\t\t\t%s\t\t\t%s\n", stk, ip, act);
+            check();
+        }
+        else
+        {
+            stk[i] = ip[j];
+            stk[i + 1] = '\0';
+            ip[j] = ' ';
+            printf("$%s\t\t\t%s\t\t\t%s\n", stk, ip, act);
+            check();
         }
     }
 }
