7664a3b174d4ba8c888ace9001ab0f1d75d421ed
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-12-13 16:45:01 +0530

    code

--- external/recursive descent/main.c
+++ external/recursive descent/main.c
@@ -1,94 +1,112 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 char ip[20];
 char *cursor;
-
-int E(),Edash(),T(),Tdash(),F();
-
-int main(){
-    printf("Grammar: \n E->E+T|T\nT->T*F|F\nF->(E) | i\n");
+int E(), Edash(), T(), Tdash(), F();
+int main()
+{
+    printf("Grammar: E->E+T | T\nT->T*F|F\nF->(E)|i\n");
     printf("Enter the expression: ");
-    scanf("%s",ip);
-    cursor=ip;
-    if(E() && *cursor=='\0'){
-        printf("Accepted\n");
+    scanf("%s", ip);
+    cursor = ip;
+    if (E() && *cursor == '\0')
+    {
+        printf("Accept\n");
         return 0;
     }
-    else{
+    else
+    {
         printf("Rejected\n");
-        exit(1);
+        return 1;
     }
-
 }
 
-
-int E(){
-    if(T()){
-        if(Edash()){
-            printf("\nE->TE'");
+int E()
+{
+    if (T())
+    {
+        if (Edash())
+        {
+            printf("E->TE'\n");
             return 1;
         }
         return 0;
     }
     return 0;
 }
-int Edash(){
-    if(*cursor=='+'){
+int Edash()
+{
+    if (*cursor == '+')
+    {
         cursor++;
-        if(T()){
-            if(Edash()){
-                printf("\nE'->+TE'");
+        if (T())
+        {
+            if (Edash())
+            {
+                printf("E'->+TE'\n");
                 return 1;
             }
             return 0;
         }
         return 0;
     }
-    printf("\nE'->e");
+    printf("E'->e\n");
     return 1;
 }
-int T(){
-    if(F()){
-        if(Tdash()){
-            printf("\nT->FT'");
+int T()
+{
+    if (F())
+    {
+        if (Tdash())
+        {
+            printf("T->FT'\n");
             return 1;
         }
         return 0;
     }
     return 0;
 }
-int Tdash(){
-    if(*cursor=='*'){
+int Tdash()
+{
+    if (*cursor == '*')
+    {
         cursor++;
-        if(F()){
-            if(Tdash()){
-                printf("\nT'->*FT'");
+        if (F())
+        {
+            if (Tdash())
+            {
+                printf("T'->*FT'\n");
                 return 1;
             }
             return 0;
         }
         return 0;
     }
-    printf("\nT'->e");
+    printf("T'->e\n");
     return 1;
 }
-int F(){
-    if(*cursor=='('){
+int F()
+{
+    if (*cursor == '(')
+    {
         cursor++;
-        if(E()){
-            if(*cursor==')'){
-                printf("\nF->(E)");
+        if (E())
+        {
+            if (*cursor == ')')
+            {
                 cursor++;
+                printf("F->(E)\n");
                 return 1;
             }
             return 0;
         }
         return 0;
     }
-    else if(*cursor=='i'){
-        printf("\nF->i");
+    else if (*cursor == 'i')
+    {
+        printf("F->i\n");
         cursor++;
         return 1;
     }
