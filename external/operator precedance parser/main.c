2a36028830a51e9af8d5f7dd211394bc98b32e73
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-12-14 08:17:39 +0530

    code

--- external/operator precedance parser/main.c
+++ external/operator precedance parser/main.c
@@ -4,72 +4,86 @@
 
 int main()
 {
-  char opt[20][20][1], stack[20], ter[20], input[20];
-  int top = 0, col = 0, row = 0, n, k = 0;
-  stack[top] = '$';
-  printf("Enter the terminals: ");
+  int n, k = 0, row = 0, col = 0, top = 0;
+  char opt[20][20][1], ter[20], ip[20], stack[20];
+  printf("Enter the terminals: \n");
   scanf("%s", ter);
   n = strlen(ter);
-  printf("Enter the table values: \n");
+  printf("Enter the precedence table: \n");
   for (int i = 0; i < n; i++)
   {
     for (int j = 0; j < n; j++)
     {
-      printf("Enter the value for %c %c : ", ter[i], ter[j]);
+      printf("Enter the value for %c %c: ", ter[i], ter[j]);
       scanf("%s", opt[i][j]);
     }
   }
+  printf("\nOperator precedence table:\n");
+  for (int i = 0; i < n; i++)
+  {
+    printf("%c\t\t", ter[i]);
+  }
+  printf("\n");
 
-  printf("Operator precedence table: \n");
   for (int i = 0; i < n; i++)
   {
     for (int j = 0; j < n; j++)
     {
-      printf("\t%c", opt[i][j][0]);
+      printf("%c\t\t", opt[i][j][0]);
     }
     printf("\n");
   }
 
-  printf("Enter the input string: \n");
-  scanf("%s", input);
-  printf("STACK\t\tINPUT\t\tACTION\n");
-  printf("%s\t\t%s\t\t",stack,input);
-  while (k <= strlen(input))
+  stack[top] = '$';
+  printf("Enter the input: ");
+  scanf("%s", ip);
+  while (k <= strlen(ip))
   {
-    for(int i=0;i<n;i++){
-      if(stack[top]==ter[i]){
-        col=i;
+    for (int i = 0; i < n; i++)
+    {
+      if (stack[top] == ter[i])
+      {
+        col = i;
       }
-      if(ter[i]==input[k]){
-        row=i;
+      if (ter[i] == ip[k])
+      {
+        row = i;
       }
     }
-    if(stack[top]=='$' && input[k]=='$'){
-      printf("Accepted..\n");
+
+    if ((stack[top] == '$') &&( ip[k] == '$'))
+    {
+      printf("Accept..\n");
       return 0;
     }
-    else if((opt[col][row][0]=='<') || (opt[col][row][0]=='=')){
-      stack[++top]=opt[col][row][0];
-      stack[++top]=input[k];
+    else if ((opt[col][row][0] == '<') || (opt[col][row][0] == '='))
+    {
+      stack[++top] = opt[col][row][0];
+      stack[++top] = ip[k];
       k++;
       printf("SHIFT\n");
     }
-    else{
-      if(opt[col][row][0]=='>'){
-        while(stack[top]!='<'){
+    else
+    {
+      if (opt[col][row][0] == '>')
+      {
+        while (stack[top] != '<')
+        {
           top--;
         }
-        top=top-1;
+        top = top - 1;
         printf("Reduce\n");
       }
-      else{
-        printf("Rejected..\n");
-        exit(0);
+      else
+      {
+        printf("reject\n");
+        return 1;
       }
     }
-    printf("%s\t\t",stack);
-    for(int i=k;i<n;i++){
-      printf("%c",input[i]);
+    printf("%s\t\t", stack);
+    for (int i = k; i < n; i++)
+    {
+      printf("%c", ip[i]);
     }
     printf("\t\t");
   }
--- external/shift reduce/main.c
+++ external/shift reduce/main.c
@@ -2,14 +2,16 @@
 #include<stdlib.h>
 #include<string.h>
 
-char ip[20],stack[20],shift[20],reduce[20];
-int top,i=0,j=0,z=0,n;
+
+int z,i=0,j=0,n;
+char stack[20],input[20],shift[20],reduce[20];
+
 void check(){
     for(z=0;z<n;z++){
         if(stack[z]=='i' && stack[z+1]=='d'){
             stack[z]='E';
             stack[z+1]='\0';
-            printf("$%s\t\t%s\t\t%s\n",stack,ip,reduce);
+            printf("$%s\t\t%s\t\t%s\n",stack,input,reduce);
             j++;
         }
     }
@@ -18,16 +20,16 @@ void check(){
             stack[z]='E';
             stack[z+1]='\0';
             stack[z+2]='\0';
-            printf("$%s\t\t%s\t\t%s\n",stack,ip,reduce);
+            printf("$%s\t\t%s\t\t%s\n",stack,input,reduce);
             i--;
         }
     }
     for(z=0;z<n;z++){
-        if(stack[z]=='E' && stack[z+1]=='+' && stack[z+2]=='E'){
+        if(stack[z]=='E' && stack[z+1]=='*' && stack[z+2]=='E'){
             stack[z]='E';
             stack[z+1]='\0';
             stack[z+2]='\0';
-            printf("$%s\t\t%s\t\t%s\n",stack,ip,reduce);
+            printf("$%s\t\t%s\t\t%s\n",stack,input,reduce);
             i--;
         }
     }
@@ -36,7 +38,7 @@ void check(){
             stack[z]='E';
             stack[z+1]='\0';
             stack[z+2]='\0';
-            printf("$%s\t\t%s\t\t%s\n",stack,ip,reduce);
+            printf("$%s\t\t%s\t\t%s\n",stack,input,reduce);
             i--;
         }
     }
@@ -44,24 +46,24 @@ void check(){
 int main(){
     strcpy(shift,"SHIFT");
     strcpy(reduce,"REDUCE");
-    printf("Grammar: E->E+E|E*E|E/E|i\n");
-    printf("Enter the ip string: ");
-    scanf("%s",ip);
-    n=strlen(ip);
-    for(i=0,j=0;j<n;j++,i++){
-        if(ip[j]=='i' && ip[j+1]=='d'){
-            stack[i]=ip[j];
-            stack[i+1]=ip[j+1];
+    printf("Grammar: E->E+E | E*E | E/E | id\n");
+    printf("Enter the string: ");
+    scanf("%s",input);
+    n=strlen(input);
+    for(i=0,j=0;j<n;i++,j++){
+        if(input[j]=='i' && input[j+1]=='d'){
+            stack[i]=input[j];
+            stack[i+1]=input[j+1];
             stack[i+2]='\0';
-            ip[j]=ip[j+1]=' ';
-            printf("$%s\t\t%s\t\t%s\n",stack,ip,shift);
+            input[j]=input[j+1]=' ';
+            printf("$%s\t\t%s\t\t%s\n",stack,input,shift);
             check();
         }
         else{
-            stack[i]=ip[j];
+            stack[i]=input[j];
             stack[i+1]='\0';
-            ip[j]=' ';
-            printf("$%s\t\t%s\t\t%s\n",stack,ip,shift);
+            input[j]=' ';
+            printf("$%s\t\t%s\t\t%s\n",stack,input,shift);
             check();
         }
     }
